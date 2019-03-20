806d5410913d906904ba5e2005fdb66a0c67075d
gbox3d <gbox3d@gmail.com>
2019-03-20 09:04:38 +0900

    라우터 샘플수정

--- ex_05_router/client/main.js
+++ ex_05_router/client/main.js
@@ -1,12 +1,13 @@
 import './header.html'
 import './footer.html'
 
+import './main.html'
+import './layout.html'
+import '../lib/router'
+
 import './index'
 import './master'
 import './detail'
-
-import './layout.html'
-import '../lib/router'
 
 
 
