30d5e4487bce3d2c15b72c68e823a0ee3c08aa65
gbox3d <gbox3d@gmail.com>
2019-12-02 00:51:44 +0900

    1.8.2 update and skip 문 예제 추가

--- ex_07_collection/client/index.js
+++ ex_07_collection/client/index.js
@@ -1,20 +1,25 @@
-const moment = require('moment')
+
+import '../lib/collections'
+
+// const moment = require('moment')
+// import moment from 'moment'
 //서버측에서 발행한 내용에 대한 구독을 꼭 해주어야한다.
 
+import './index.html'
 
-
-let handlerMsgLog
+// let handlerMsgLog
 Meteor.startup(function () {
 
   console.log('start app');
   //미티어 전역에서 구독
-  handlerMsgLog = Meteor.subscribe('msgLog/list',{_limit:10});
+
 });
 
 
 Template.main.onCreated(function () {
   //탬플리트 단위로 구독
   //this.subscribe('msgLog/list');
+  this.handlerMsgLog = Meteor.subscribe('msgLog/list',{_limit:5});
   console.log(this)
 
   // console.log(moment().format())
@@ -26,23 +31,23 @@ Template.main.onCreated(function () {
 
 Template.main.helpers({
   "getMsgLogList" : function () {
-//collections.js 에서 MsgLog 선언된것을 사용함.
-    //fetch를 사용하면 안된다.
-    return MsgLog.find({} ,
-      {
-        sort : {
-          createdAt : -1
-        },
-      });
-  },
-  "getSetting" () {
-    return Meteor.settings
+
+    //collections.js 에서 MsgLog 선언된것을 사용함.
+    //find, findone 만을 사용해야 리액티브가 발동한다. fetch를 사용하면 안된다.
+
+    return MsgLog.find({},
+        {
+          sort : {
+            createdAt : -1
+          }
+        });
   }
 
 });
 
 Template.main.events({
   "click .btn-update"(evt,instance) {
+    //메씨지 수정창 보이
     console.log(instance);
     console.log(this);
 
@@ -51,7 +56,9 @@ Template.main.events({
     instance.$('.input-'+ this._id + ' input').val(this.msg)
 
   },
-  "click .btn-update-ok"(evt,instance) {
+  "click .btn-update-ok"(evt,instance) {기
+
+    //메씨지 수정
     console.log(instance);
     console.log(this);
 
@@ -64,10 +71,12 @@ Template.main.events({
 
   },
   "click .btn-del"(evt,instance) {
+    //지우기
     console.log('del ' + this._id)
     Meteor.call('msgLog/remove',{_id:this._id});
   },
   "submit #addMsg"(evt,instance) {
+    //메씨지 추가
 
     try {
 
@@ -84,11 +93,15 @@ Template.main.events({
   "submit #setLimit"(evt,instance) {
     console.log(evt.target.inputLimit.value)
 
-    handlerMsgLog.stop()
+    //구독취소
+    instance.handlerMsgLog.stop()
 
-    handlerMsgLog = Meteor.subscribe('msgLog/list', {
-      _limit : parseInt(evt.target.inputLimit.value)
+    //범위 다시 지정하여 재구독
+    instance.handlerMsgLog = Meteor.subscribe('msgLog/list', {
+      _limit : parseInt(evt.target.Limit.value),
+      skip : parseInt(evt.target.Skip.value)
     });
+
     evt.preventDefault()
   }
 
--- ex_07_collection/server/main.js
+++ ex_07_collection/server/main.js
@@ -1,4 +1,6 @@
 
+import '../lib/collections'
+
 const moment = require('moment')
 
 Meteor.startup(function () {
@@ -7,25 +9,25 @@ Meteor.startup(function () {
 
   console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
 
-
-
 });
 
+//콜랙션 발간
 Meteor.publish('msgLog/list',
-  function ({_limit}) {
-  console.log(_limit)
-    return MsgLog.find({
+    function ({_limit,skip}) {
+      console.log(_limit)
+      return MsgLog.find({
 
-        createdAt : {$gt : new Date(moment('2018-10-29 09:00')) }
+            createdAt : {$gt : new Date(moment('2018-10-29 09:00')) }
 
-      },
-      {
-        sort : {
-          createdAt : -1 // 최신것 부터 받아가기
-        },
-        limit:_limit //갯수 지정
-      })
-  }
+          },
+          {
+            sort : {
+              createdAt : -1 // 최신것 부터 받아가기
+            },
+              limit:_limit, //갯수 지정
+              skip : skip
+          })
+    }
 );
 
 Meteor.methods({
